f4d0860d912bcdafb4f97448e99eb6ba7fe220f0
Matthew Collins <m.collins@etoncollege.org.uk>
2025-01-08 22:19:18 +0000

    complete example

--- Static vs Instance Members/main.swift
+++ Static vs Instance Members/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ - Static members belong to the type and provide shared information or functionality.
+ - Instance members belong to a specific object and hold data relevant to that instance.
+ */
 
+
+struct EtonDiv {
+    var numberOfStudents: Int = 0
+    static let maxSize: Int = 2
+    
+    mutating func addStudent() {
+        if numberOfStudents < EtonDiv.maxSize {
+            self.numberOfStudents += 1
+            print("Successfully added student")
+        }
+        else {
+            print("Div at max capacity!")
+        }
+    }
+}
+
+
+var divA = EtonDiv()
+divA.addStudent()
+divA.addStudent()
+divA.addStudent()
+
+var divB = EtonDiv()
+divB.addStudent()
